cb6a5abde39c0cc6e8b73e80c993c19507d2b56b
umalemaors <56729297+umalemaors@users.noreply.github.com>
2025-09-26 10:59:09 -0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// URL única do seu contador
-const contadorURL = "https://api.countapi.xyz/hit/seu-site-qr-code/visitas";
+// URL única para o contador
+const contadorURL = "https://api.countapi.xyz/hit/umalemaors.github.io/meu-site-qr/visitas";
 
-// Função para atualizar o contador
+// Atualiza o contador quando a página é carregada
 fetch(contadorURL)
   .then(response => response.json())
   .then(data => {
     document.getElementById('contador').innerText = data.value;
-  });
+  })
+  .catch(error => console.error('Erro ao atualizar contador:', error));
