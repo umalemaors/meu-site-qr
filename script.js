277385c958fa627dc3bb98fd0328ef34c2d5faa7
umalemaors <56729297+umalemaors@users.noreply.github.com>
2025-09-26 13:03:40 -0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // URL correta com key
 const contadorURL = "https://script.google.com/macros/s/AKfycbwzq550kPLXxNx4z16RD3XnDIX61DlM09ZHxTgY8Rmj1-TOB3Jz_800yIlTcDogDjHCYQ/exec";
-
 fetch(contadorURL)
   .then(response => response.json())
   .then(data => {
